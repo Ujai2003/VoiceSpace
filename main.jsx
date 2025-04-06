91bbc35ce5c1d495ac60852447982fc73894bba3
Ujai2003 <142999556+Ujai2003@users.noreply.github.com>
2025-04-06 19:39:59 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,10 @@
+// /src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import VoiceScapePrototype from './VoiceScapePrototype'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <VoiceScapePrototype />
+  </React.StrictMode>
+)
