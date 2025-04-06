bbff7f9b25f52f4b2a7ba8a8010fd5a5015a8dab
Ujai2003 <bcujai2003@gmail.com>
2025-04-06 20:47:03 +0530

    initial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VoiceScapePrototype from './components/VoiceScapePrototype';
+
+function App() {
+    return (
+      <div className="App">
+        <VoiceScapePrototype />
+      </div>
+    );
+  }
+  
+
+export default App;
